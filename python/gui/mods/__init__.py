b1dd2281201503a389794faa6f162084aaeba127
Dictor1488 <168811367+Dictor1488@users.noreply.github.com>
2026-04-18 16:31:19 +0300

    Update __init__.py

--- python/gui/mods/__init__.py
+++ python/gui/mods/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-# Маркер пакета gui.mods + заглушки init/fini,
-# яких очікує WoT mod loader.
+# Маркер пакета gui.mods + повний набір заглушок WoT mod loader API.
+# WoT перебирає всі .py файли в gui/mods/ і викликає у кожному:
+#   init(), fini(), sendEvent(), onHangarSpaceCreate(), onHangarSpaceDestroy()
+# Якщо якоїсь функції немає — клієнт падає з AttributeError.
 
 
 def init():
@@ -8,4 +10,16 @@ def init():
 
 
 def fini():
+    pass
+
+
+def sendEvent(*args, **kwargs):
+    pass
+
+
+def onHangarSpaceCreate(*args, **kwargs):
+    pass
+
+
+def onHangarSpaceDestroy(*args, **kwargs):
     pass
